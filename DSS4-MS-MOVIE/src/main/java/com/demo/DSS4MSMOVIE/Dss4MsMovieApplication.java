02ef46da3d39028cede7b81fea393ab9b9add54e
JANDSSGITHUB <jandssgithub@gmail.com>
2022-10-25 13:36:01 +0800

    Add movie microservice

---
+++ DSS4-MS-MOVIE/src/main/java/com/demo/DSS4MSMOVIE/Dss4MsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.demo.DSS4MSMOVIE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss4MsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dss4MsMovieApplication.class, args);
+	}
+
+}
---
+++ DSS4-MS-MOVIE/src/test/java/com/demo/DSS4MSMOVIE/Dss4MsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.DSS4MSMOVIE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss4MsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
