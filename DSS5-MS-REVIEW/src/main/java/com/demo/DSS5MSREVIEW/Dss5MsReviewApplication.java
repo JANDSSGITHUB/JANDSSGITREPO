4ba5b081a09b2f9c532941e5ed63f8f8aee102f8
JANDSSGITHUB <jandssgithub@gmail.com>
2022-10-25 13:40:08 +0800

    Add Review microservice

---
+++ DSS5-MS-REVIEW/src/main/java/com/demo/DSS5MSREVIEW/Dss5MsReviewApplication.java
@@ -0,0 +1,13 @@
+package com.demo.DSS5MSREVIEW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss5MsReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dss5MsReviewApplication.class, args);
+	}
+
+}
---
+++ DSS5-MS-REVIEW/src/test/java/com/demo/DSS5MSREVIEW/Dss5MsReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.DSS5MSREVIEW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss5MsReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
