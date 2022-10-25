c94884100375160d941b9b9fa01e4c0354323311
JANDSSGITHUB <jandssgithub@gmail.com>
2022-10-25 13:38:26 +0800

    Add Actor microservice

---
+++ DSS5-MS-ACTOR/src/main/java/com/demo/DSS5MSACTOR/Dss5MsActorApplication.java
@@ -0,0 +1,13 @@
+package com.demo.DSS5MSACTOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dss5MsActorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dss5MsActorApplication.class, args);
+	}
+
+}
---
+++ DSS5-MS-ACTOR/src/test/java/com/demo/DSS5MSACTOR/Dss5MsActorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.DSS5MSACTOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dss5MsActorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
